5afb540aa73d6a76cca57d1ac2e5dfc5ac2e9304
JelleWesterbos <jellewesterbos@gmail.com>
2019-05-02 01:00:58 +0200

    Updated

--- Homework/week3/converter.py
+++ Homework/week3/converter.py
@@ -4,8 +4,8 @@ import json
 # Open the CSV
 f = open('global-plastics-production.csv', 'rU')
 
-# Change each fieldname to the appropriate field name. I know, so difficult.
-reader = csv.DictReader( f, fieldnames = ( "Entity","Code","Year","Global plastics production (million tonnes) (tonnes)" ))
+# Change each fieldname to the appropriate field name.
+reader = csv.DictReader( f, fieldnames = ( "Entity","Code","Year","Production" ))
 
 # Parse the CSV into JSON
 out = json.dumps( [ row for row in reader ] )
